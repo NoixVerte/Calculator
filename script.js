6942792963f25366a1f98ca348aa31853aedd02a
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-15 11:18:06 +0200

    Add keyboard support

--- script.js
+++ script.js
@@ -1,93 +1,185 @@
 const resultScreen = document.querySelector("#result-screen").firstElementChild;
 const buttonsWrapper = document.querySelector("#buttons");
+const page = document.querySelector("body");
 const buttons = document.querySelectorAll("button");
 let num1 = "";
 let num2 = "";
 let operator = "";
 resultScreen.innerText = "";
 
+
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        switch(button.innerText) {
-            case "C":
-                wipeResultScreen();
+    button.addEventListener("click", handleButtonPress);
+});
+
+page.addEventListener("keydown", handleKeyPress);
+
+function handleButtonPress() {
+    switch(this.innerText) {
+        case "C":
+            wipeResultScreen();
+            flushVariables();
+            break;
+        case "=":
+            if (num1 !== "" && operator !== "" && (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0)) {
+                num2 = assignNumber(resultScreen.innerText);
+                resultScreen.innerText = operate();
                 flushVariables();
-                break;
-            case "=":
-                if (num1 !== "" && operator !== "" && (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0)) {
+            }
+            break;
+        case "+/-":
+            if (parseFloat(resultScreen.innerText) < 0) {
+                resultScreen.innerText = Math.abs(resultScreen.innerText);
+            } else if (parseFloat(resultScreen.innerText) > 0){
+                resultScreen.innerText = -Math.abs(resultScreen.innerText);
+            }
+            break;
+        case "<─":
+            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                if (resultScreen.innerText.length > 0) {
+                    resultScreen.innerText = resultScreen.innerText.slice(0,-1);
+                }
+            }
+            break;
+        case ".":
+            if (!resultScreen.innerText.includes(".")) {
+                if (resultScreen.innerText === "") {
+                    resultScreen.innerText = "0.";
+                } else if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                    updateResultScreen(this.innerText);
+                }
+            }
+            break;
+        case "+":
+        case "-":
+        case "x":
+        case "/":
+            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                if (num1 === "") {
+                    num1 = assignNumber(resultScreen.innerText);
+                    wipeResultScreen();
+                    resultScreen.innerText = this.innerText;
+                } else if (num2 === "") {
                     num2 = assignNumber(resultScreen.innerText);
                     resultScreen.innerText = operate();
                     flushVariables();
-                }
-                break;
-            case "+/-":
-                if (parseFloat(resultScreen.innerText) < 0) {
-                    resultScreen.innerText = Math.abs(resultScreen.innerText);
-                } else if (parseFloat(resultScreen.innerText) > 0){
-                    resultScreen.innerText = -Math.abs(resultScreen.innerText);
-                }
-                break;
-            case "<─":
-                if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                    if (resultScreen.innerText.length > 0) {
-                        resultScreen.innerText = resultScreen.innerText.slice(0,-1);
-                    }
-                }
-                break;
-            case ".":
-                if (!resultScreen.innerText.includes(".")) {
-                    if (resultScreen.innerText === "") {
-                        resultScreen.innerText = "0.";
-                    } else if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                        updateResultScreen(button.innerText);
-                    }
-                }
-                break;
-            case "+":
-            case "-":
-            case "x":
-            case "/":
-                if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                    if (num1 === "") {
-                        num1 = assignNumber(resultScreen.innerText);
-                        wipeResultScreen();
-                        resultScreen.innerText = button.innerText;
-                    } else if (num2 === "") {
-                        num2 = assignNumber(resultScreen.innerText);
-                        resultScreen.innerText = operate();
-                        flushVariables();
-                        num1 = assignNumber(resultScreen.innerText);
-                        wipeResultScreen();
-                        resultScreen.innerText = button.innerText;
-                    }
-                }
-                break;
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-            case "0":
-                if (resultScreen.innerText === "Nope!") wipeResultScreen();
-                if (num1 !== "" && (resultScreen.innerText == "+" || resultScreen.innerText == "-" || resultScreen.innerText == "x" || resultScreen.innerText == "/")) {
-                    assignOperator(resultScreen.innerText);
+                    num1 = assignNumber(resultScreen.innerText);
                     wipeResultScreen();
+                    resultScreen.innerText = this.innerText;
                 }
-                if (resultScreen.innerText.length < 12) {
-                    if (resultScreen.innerText === "0") {
-                        resultScreen.innerText = button.innerText;
-                    } else  {
-                        updateResultScreen(button.innerText);
-                    }
+            }
+            break;
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            if (resultScreen.innerText === "Nope!") wipeResultScreen();
+            if (num1 !== "" && (resultScreen.innerText == "+" || resultScreen.innerText == "-" || resultScreen.innerText == "x" || resultScreen.innerText == "/")) {
+                assignOperator(resultScreen.innerText);
+                wipeResultScreen();
+            }
+            if (resultScreen.innerText.length < 12) {
+                if (resultScreen.innerText === "0") {
+                    resultScreen.innerText = this.innerText;
+                } else  {
+                    updateResultScreen(this.innerText);
                 }
-                break;
-        }
-    })
-});
+            }
+            break;
+    }
+}
+
+function handleKeyPress(event) {
+    console.log("Function reached");
+    console.log(`${event.key} key pressed!`);
+    switch(String(event.key)) {
+        case "C":
+        case "c":
+            wipeResultScreen();
+            flushVariables();
+            break;
+        case "=":
+        case "Enter":
+            if (num1 !== "" && operator !== "" && (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0)) {
+                num2 = assignNumber(resultScreen.innerText);
+                resultScreen.innerText = operate();
+                flushVariables();
+            }
+            break;
+        case "+/-":
+            if (parseFloat(resultScreen.innerText) < 0) {
+                resultScreen.innerText = Math.abs(resultScreen.innerText);
+            } else if (parseFloat(resultScreen.innerText) > 0){
+                resultScreen.innerText = -Math.abs(resultScreen.innerText);
+            }
+            break;
+        case "<─":
+        case "Backspace":
+            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                if (resultScreen.innerText.length > 0) {
+                    resultScreen.innerText = resultScreen.innerText.slice(0,-1);
+                }
+            }
+            break;
+        case ".":
+            if (!resultScreen.innerText.includes(".")) {
+                if (resultScreen.innerText === "") {
+                    resultScreen.innerText = "0.";
+                } else if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                    updateResultScreen(event.key);
+                }
+            }
+            break;
+        case "+":
+        case "-":
+        case "x":
+        case "/":
+            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
+                if (num1 === "") {
+                    num1 = assignNumber(resultScreen.innerText);
+                    wipeResultScreen();
+                    resultScreen.innerText = event.key;
+                } else if (num2 === "") {
+                    num2 = assignNumber(resultScreen.innerText);
+                    resultScreen.innerText = operate();
+                    flushVariables();
+                    num1 = assignNumber(resultScreen.innerText);
+                    wipeResultScreen();
+                    resultScreen.innerText = event.key;
+                }
+            }
+            break;
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            if (resultScreen.innerText === "Nope!") wipeResultScreen();
+            if (num1 !== "" && (resultScreen.innerText == "+" || resultScreen.innerText == "-" || resultScreen.innerText == "x" || resultScreen.innerText == "/")) {
+                assignOperator(resultScreen.innerText);
+                wipeResultScreen();
+            }
+            if (resultScreen.innerText.length < 12) {
+                if (resultScreen.innerText === "0") {
+                    resultScreen.innerText = event.key;
+                } else  {
+                    updateResultScreen(event.key);
+                }
+            }
+            break;
+    }
+}
 
 function operate() {
     switch(operator) {
