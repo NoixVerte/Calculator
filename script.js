96dfcc2c74bafb76d054d45c4b74188dc153c770
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-17 14:44:24 +0200

    Fix bug related to combining buton inputs and keyb oard inputs Reduce code redundancy

--- script.js
+++ script.js
@@ -11,91 +11,11 @@ buttons.forEach(button => {
     button.addEventListener("click", handleButtonPress);
 });
 
-page.addEventListener("keydown", handleKeyPress);
+page.addEventListener("keydown", handleButtonPress);
 
-function handleButtonPress() {
-    switch(this.innerText) {
-        case "C":
-            wipeResultScreen();
-            flushVariables();
-            break;
-        case "=":
-            if (num1 !== "" && operator !== "" && (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0)) {
-                num2 = assignNumber(resultScreen.innerText);
-                resultScreen.innerText = operate();
-                flushVariables();
-            }
-            break;
-        case "+/-":
-            if (parseFloat(resultScreen.innerText) < 0) {
-                resultScreen.innerText = Math.abs(resultScreen.innerText);
-            } else if (parseFloat(resultScreen.innerText) > 0){
-                resultScreen.innerText = -Math.abs(resultScreen.innerText);
-            }
-            break;
-        case "<─":
-            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                if (resultScreen.innerText.length > 0) {
-                    resultScreen.innerText = resultScreen.innerText.slice(0,-1);
-                }
-            }
-            break;
-        case ".":
-            if (!resultScreen.innerText.includes(".")) {
-                if (resultScreen.innerText === "") {
-                    resultScreen.innerText = "0.";
-                } else if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                    updateResultScreen(this.innerText);
-                }
-            }
-            break;
-        case "+":
-        case "-":
-        case "*":
-        case "/":
-            if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                if (num1 === "") {
-                    num1 = assignNumber(resultScreen.innerText);
-                    wipeResultScreen();
-                    resultScreen.innerText = this.innerText;
-                } else if (num2 === "") {
-                    num2 = assignNumber(resultScreen.innerText);
-                    resultScreen.innerText = operate();
-                    flushVariables();
-                    num1 = assignNumber(resultScreen.innerText);
-                    wipeResultScreen();
-                    resultScreen.innerText = this.innerText;
-                }
-            }
-            break;
-        case "1":
-        case "2":
-        case "3":
-        case "4":
-        case "5":
-        case "6":
-        case "7":
-        case "8":
-        case "9":
-        case "0":
-            if (resultScreen.innerText === "Nope!") wipeResultScreen();
-            if (num1 !== "" && (resultScreen.innerText == "+" || resultScreen.innerText == "-" || resultScreen.innerText == "*" || resultScreen.innerText == "/")) {
-                assignOperator(resultScreen.innerText);
-                wipeResultScreen();
-            }
-            if (resultScreen.innerText.length < 12) {
-                if (resultScreen.innerText === "0") {
-                    resultScreen.innerText = this.innerText;
-                } else  {
-                    updateResultScreen(this.innerText);
-                }
-            }
-            break;
-    }
-}
-
-function handleKeyPress(event) {
-    switch(String(event.key)) {
+function handleButtonPress(event) {
+    const input = event.key || this.innerText;
+    switch(input) {
         case "C":
         case "c":
             wipeResultScreen();
@@ -129,7 +49,7 @@ function handleKeyPress(event) {
                 if (resultScreen.innerText === "") {
                     resultScreen.innerText = "0.";
                 } else if (parseFloat(resultScreen.innerText) || parseFloat(resultScreen.innerText) === 0) {
-                    updateResultScreen(event.key);
+                    updateResultScreen(event.key || this.innerText);
                 }
             }
             break;
@@ -141,14 +61,14 @@ function handleKeyPress(event) {
                 if (num1 === "") {
                     num1 = assignNumber(resultScreen.innerText);
                     wipeResultScreen();
-                    resultScreen.innerText = event.key;
+                    resultScreen.innerText = event.key || this.innerText;
                 } else if (num2 === "") {
                     num2 = assignNumber(resultScreen.innerText);
                     resultScreen.innerText = operate();
                     flushVariables();
                     num1 = assignNumber(resultScreen.innerText);
                     wipeResultScreen();
-                    resultScreen.innerText = event.key;
+                    resultScreen.innerText = event.key || this.innerText;
                 }
             }
             break;
@@ -169,9 +89,9 @@ function handleKeyPress(event) {
             }
             if (resultScreen.innerText.length < 12) {
                 if (resultScreen.innerText === "0") {
-                    resultScreen.innerText = event.key;
+                    resultScreen.innerText = event.key || this.innerText;
                 } else  {
-                    updateResultScreen(event.key);
+                    updateResultScreen(event.key || this.innerText);
                 }
             }
             break;
